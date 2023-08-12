cd954d1de54a958e6dbefb56fd0384249ec51f69
callmechelsea <133617421+callmechelsea@users.noreply.github.com>
2023-08-12 16:12:09 -0700

    fixed errors of add book and remove book

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,18 +1,19 @@
 // const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
-const { signToken, AuthenticationError } = require('../utils/auth');
+const { User } = require("../models");
+const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
-        if(context.user) {
-            const userData = await User.findOne({
-                _id: context.user._id});
-            return userData;
-        }
-        throw AuthenticationError;
-    }
-},  
+      if (context.user) {
+        const userData = await User.findOne({
+          _id: context.user._id,
+        });
+        return userData;
+      }
+      throw AuthenticationError;
+    },
+  },
 
   Mutation: {
     addUser: async (parent, { username, email, password }) => {
@@ -38,38 +39,35 @@ const resolvers = {
 
       return { token, userData };
     },
-},
 
     saveBook: async (parent, { bookData }, context) => {
       if (!context.user) {
         throw AuthenticationError;
-        } 
-        else {
+      } else {
         const updatedUser = await User.findByIdAndUpdate(
-            { _id: context.user._id },
-            { $push: { savedBooks: bookData }},
-            { new: true }
+          { _id: context.user._id },
+          { $push: { savedBooks: bookData } },
+          { new: true }
         );
 
         return updatedUser;
-        }
+      }
     },
-
 
     removeBook: async (parent, { bookId }, context) => {
-        if (!context.user) {
-            throw AuthenticationError;
-        }
-        else {
+      if (!context.user) {
+        throw AuthenticationError;
+      } else {
         const updatedUser = await User.findOneAndUpdate(
-            { _id: context.user._id },
-            { $pull: { savedBooks: book }},
-            { new: true }
+          { _id: context.user._id },
+          { $pull: { savedBooks: book } },
+          { new: true }
         );
 
         return updatedUser;
-        };
+      }
     },
+  },
 };
 
 module.exports = resolvers;
