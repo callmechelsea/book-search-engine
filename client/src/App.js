5aa6a263aaa28be55dc0438496de71dd30ef31a2
callmechelsea <133617421+callmechelsea@users.noreply.github.com>
2023-08-12 16:56:27 -0700

    changed back file

--- client/src/App.js
+++ client/src/App.js
@@ -1,56 +1,21 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-// import SearchBooks from './pages/SearchBooks';
-// import SavedBooks from './pages/SavedBooks';
-// import Navbar from './components/Navbar';
-
-// function App() {
-//   return (
-//     <Router>
-//       <>
-//         <Navbar />
-//         <Switch>
-//           <Route exact path='/' component={SearchBooks} />
-//           <Route exact path='/saved' component={SavedBooks} />
-//           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-//         </Switch>
-//       </>
-//     </Router>
-//   );
-// }
-
-// export default App;
-import { Outlet } from 'react-router-dom';
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
-import Navbar from './components/Navbar'
-import { setContext } from '@apollo/client/link/context';
-
-const httpLink = createHttpLink({
-  uri: 'graphql',
-});
-
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-
-  return {
-    headers:{
-      ...headers,
-      authorization: token ? `Bearer ${token}`: '',
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <Navbar />
-      <Outlet />
-    </ApolloProvider>
+    <Router>
+      <>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={SearchBooks} />
+          <Route exact path='/saved' component={SavedBooks} />
+          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+        </Switch>
+      </>
+    </Router>
   );
 }
 
